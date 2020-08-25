7429bbde6dfa88f2ddd1a028419cfa5bd283b83b
Paper Diamond <paperdiamond@users.noreply.github.com>
2020-08-25 22:13:13 +1200

    Updates
    Updates from tonights study. Found Dan Bader's methods for LL and combined them with some of Zed Shaw's from LMPTHW

--- linkedList/linkedList.py
+++ linkedList/linkedList.py
@@ -5,12 +5,18 @@ class Node:
         self.value = value
         self.next = None
     
-    def __repr__(self):
-        return self.data
+    # def __repr__(self):
+        # return self.data
 
 class LinkedList:
     def __init__(self, head=None):
         self.head = head
+
+    def iterate(self):
+        current = self.head
+        while current:
+            print(current.value)
+            current = current.next
 
     def append(self, new_node):
         current = self.head
@@ -46,6 +52,12 @@ class LinkedList:
         elif index == 1:
             new_node.next = self.head
             self.head = new_node
+    
+    # def delete(self, value):
+    #     current =self.head
+    #     next = current.next
+    #     current.value = next.value
+    #     current.next = next.next
 
     def delete(self, value):
         current = self.head
@@ -58,15 +70,39 @@ class LinkedList:
                 previous.next = current.next
             else:
                 self.head = current.next
+    
+    def count(self):
+        current = self.head
+        count = 0
+        while current:
+            count += 1
+            current = current.next
+        return count
+
+    def printit(self):
+        current = self.head
+        while current:
+            print(current.value)
+            current = current.next
+
+    def print_index(self, search_value):
+        current = self.head
+        count = 0
+        while current.value != search_value:
+            current = current.next
+            count += 1
+        if current.value == search_value:
+            print(count)
 
 
 
-# Test cases
-# Set up some Elements
+
+# Test cases Set up some Elements
 e1 = Node(1)
 e2 = Node(2)
 e3 = Node(3)
 e4 = Node(4)
+
 
 # Start setting up a LinkedList
 ll = LinkedList(e1)
@@ -75,20 +111,23 @@ ll.append(e3)
 
 # Test get_index
 # Should print 3
-print(ll.head.next.next.value)
+# print(ll.head.next.next.value)
 # Should also print 3
-print(ll.get_index(3).value)
+# print(ll.get_index(3).value)
 
 # Test insert
 ll.insert(e4,3)
 # Should print 4 now
-print(ll.get_index(3).value)
+# print(ll.get_index(3).value)
 
 # Test delete
 ll.delete(1)
 # Should print 2 now
-print(ll.get_index(1).value)
+# print(ll.get_index(1).value)
 # Should print 4 now
-print(ll.get_index(2).value)
+# print(ll.get_index(2).value)
 # Should print 3 now
-print(ll.get_index(3).value)
+# print(ll.get_index(3).value)
+e5 = Node(276)
+ll.append(e5)
+# print(ll.count())
---
+++ linkedList/test_linked_list_v2.py
@@ -0,0 +1,6 @@
+from linked_list_v2 import *
+
+def test_start():
+    colors = SinglyLinkedList()
+    colors.append('Fantastic')
+    assert colors.count() == 1
