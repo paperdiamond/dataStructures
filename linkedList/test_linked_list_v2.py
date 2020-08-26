e0c2a88e125c1b55bdcbfda20f20d01b09dc8b4a
Paper Diamond <paperdiamond@users.noreply.github.com>
2020-08-26 13:56:25 +1200

    Daily Update
    Updated Linked LIst Test File

--- linkedList/test_linked_list_v2.py
+++ linkedList/test_linked_list_v2.py
@@ -1,6 +1,40 @@
 from linked_list_v2 import *
 
 def test_start():
-    colors = SinglyLinkedList()
-    colors.append('Fantastic')
-    assert colors.count() == 1
+    lst = SinglyLinkedList()
+    lst.append('Fantastic')
+    assert lst.count() == 1
+
+def test_append():
+    lst = SinglyLinkedList()
+    lst.append('Fantastic')
+    lst.append('105')
+    lst.append(105)
+
+def test_prepend():
+    lst = SinglyLinkedList()
+    lst.prepend(23)
+    lst.prepend('Word choice')
+    lst.prepend('150')
+    lst.prepend(1000)
+
+def test_count():
+    lst = SinglyLinkedList()
+    lst.append('Fantastic')
+    lst.append('105')
+    lst.append(105)
+    lst.count()
+
+def test_reverse():
+    lst = SinglyLinkedList()
+    lst.append('Fantastic')
+    lst.append('105')
+    lst.append(105)
+    lst.reverse()
+
+def test_find():
+    lst = SinglyLinkedList()
+    lst.append('Fantastic')
+    lst.append('105')
+    lst.append(105)
+    lst.find('105')
